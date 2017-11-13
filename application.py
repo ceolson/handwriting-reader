89d595498ec61fb82262e224cc6f8cdc1fbd65f7
ceolson <ceolson00@gmail.com>
2017-11-12 23:06:59 -0500

    Create application.py

---
+++ application.py
@@ -0,0 +1,26 @@
+import os
+import re
+from flask import Flask, jsonify, render_template, request
+
+from cs50 import SQL
+from helpers import lookup
+
+# Configure application
+app = Flask(__name__)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///mashup.db")
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def index():
+  render_template(index.html)

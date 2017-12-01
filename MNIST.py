6a13d77eb70ee8d841dd99c37b842f031bb63281
ceolson <ceolson00@gmail.com>
2017-11-30 22:39:37 -0500

    hackathon

--- MNIST.py
+++ MNIST.py
@@ -1,19 +1,48 @@
+import numpy as np
+
+# Simple script to extract MNIST data and return array of tuples
 def get_training_data():
-    file1 = open('train-labels-idx1-ubyte', 'r')
+    file1 = open('data/train-labels-idx1-ubyte', 'r', encoding = "ISO-8859-1")
     labels = file1.read()
 
-    file2 = open('train-images-idx3-ubyte', 'r')
+    file2 = open('data/train-images-idx3-ubyte', 'r', encoding = "ISO-8859-1")
     images = file2.read()
 
-    data = []
+    label_array = []
+    image_array = []
 
     for i in range(60000):
         temp_image = []
         for j in range(28 * 28):
-            temp_image.append(images[16 + 28 * 28 * i + j]) 
-        data.append((temp_image,labels[8 + i]))
+            value = images[16 + 28 * 28 * i + j]
+            pixel_value = int(value.encode("hex"), 16)
+            temp_image.append(pixel_value)
+        label =  labels[8 + i]
+        image_array.append(temp_image)
+        label_array.append(int(label.encode("hex")))
 
-    return data
+    return (np.array(label_array),np.array(image_array))
 
-data = get_training_data()
-print(data[14])
+def get_test_data():
+    file1 = open('data/t10k-labels-idx1-ubyte', 'r', encoding = "ISO-8859-1")
+    labels = file1.read()
+
+    file2 = open('data/t10k-images-idx3-ubyte', 'r', encoding = "ISO-8859-1")
+    images = file2.read()
+
+    label_array = []
+    image_array = []
+
+    for i in range(10000):
+        temp_image = []
+        for j in range(28 * 28):
+            value = images[16 + 28 * 28 * i + j]
+            pixel_value = int(value.encode("hex"), 16)
+            temp_image.append(pixel_value)
+        label =  labels[8 + i]
+        image_array.append(temp_image)
+        label_array.append(int(label.encode("hex")))
+
+    return (np.array(label_array),np.array(image_array))
+
+a = get_training_data()
--- recognize.py
+++ recognize.py
@@ -1,54 +1,75 @@
 from random import *
 import math
 import numpy as np
+import keras
 from keras.models import Sequential
+from keras.layers import Dense, Activation
+from MNIST import get_training_data, get_test_data
 
-def sigmoid(x):
-    return exp(x) / (exp(x) + 1)
+# def sigmoid(x):
+#     return exp(x) / (exp(x) + 1)
 
-# Takes in a string containing the pixels as an array
-def recognize(image, coefficients, biases, sizes):
-    layers = Layers()
-    layers.first = [pixel for pixel in image]
+# # Takes in a string containing the pixels as an array
+# def recognize(image, coefficients, biases, sizes):
+#     layers = Layers()
+#     layers.first = [pixel for pixel in image]
 
-    sizes.first = len(layers.first)
+#     sizes.first = len(layers.first)
 
-    for layer_number in range(1, 4):
-        layer = layer_list[layer_number]
-        for i in range(sizes[layer]):
-            total = 0
-            for j in range(sizes.layer_list[layer_number - 1]):
-                total = total + layers.layer_list[layer_number - 1][j] * coefficients.layer[i][j] + biases.layer[i]
-            layers.layer.append(sigmoid(total))
+#     for layer_number in range(1, 4):
+#         layer = layer_list[layer_number]
+#         for i in range(sizes[layer]):
+#             total = 0
+#             for j in range(sizes.layer_list[layer_number - 1]):
+#                 total = total + layers.layer_list[layer_number - 1][j] * coefficients.layer[i][j] + biases.layer[i]
+#             layers.layer.append(sigmoid(total))
 
-    return layers.output
+#     return layers.output
 
-def cost(image, coefficients, biases, sizes, target):
-    output = recognize(image, coefficients, biases, sizes)
-    cost = 0
-    for i in range(10):
-        if i = target:
-            cost += abs(1 - output[i])
-        else:
-            cost += abs(output[i])
-    return cost
+# def cost(image, coefficients, biases, sizes, target):
+#     output = recognize(image, coefficients, biases, sizes)
+#     cost = 0
+#     for i in range(10):
+#         if i = target:
+#             cost += abs(1 - output[i])
+#         else:
+#             cost += abs(output[i])
+#     return cost
 
-# Initialize random coefficients and biases
+# # Initialize random coefficients and biases
 
-sizes = [None, 20, 20, 10]
-coefficients = []
-biases = []
+# sizes = [None, 20, 20, 10]
+# coefficients = []
+# biases = []
 
-for layer_number in range(1,4):
-    layer = layer_list[layer_number]
-    for i in range(sizes.layer):
-        coefficients[i].append([])
-        for j in range(sizes.layer_list[layer_number - 1]):
-            coefficients[i][j].append(randint(-1, 1))
-        biases[i].append(randint(-1, 1))
+# for layer_number in range(1,4):
+#     layer = layer_list[layer_number]
+#     for i in range(sizes.layer):
+#         coefficients[i].append([])
+#         for j in range(sizes.layer_list[layer_number - 1]):
+#             coefficients[i][j].append(randint(-1, 1))
+#         biases[i].append(randint(-1, 1))
 
-recognize(image, sizes, coefficients, biases)
+# recognize(image, sizes, coefficients, biases)
 
 
 model = Sequential()
+
+model.add(Dense(units=20, activation='relu', input_dim=28 * 28))
+model.add(Dense(units=10, activation='relu'))
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+data_pairs = get_training_data()
+
+labels = data_pairs[0]
+data = data_pairs[1]
+
+# Convert labels to categorical one-hot encoding
+one_hot_labels = keras.utils.to_categorical(labels, num_classes=10)
+
+# Train the model, iterating on the data in batches of 32 samples
+model.fit(data, one_hot_labels, epochs=10, batch_size=32)
 
